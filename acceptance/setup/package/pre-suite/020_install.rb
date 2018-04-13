898af98b10737e8bb46271339df8b1d553444f97
Michael Smith <michael.smith@puppet.com>
2018-04-13 13:21:56 -0700

    (maint) Use fixed helper from beaker-puppet
    It would previously assume it was installing puppet-agent on Windows
    hosts and do checks that failed. Now works for generic packages.

--- acceptance/setup/package/pre-suite/020_install.rb
+++ acceptance/setup/package/pre-suite/020_install.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-# Inspired by https://github.com/puppetlabs/pdk/blob/master/package-testing/pre/000_install_package.rb
-# Uses helpers from beaker-puppet to fetch build defaults
-
 test_name 'Install Bolt package' do
   if ENV['LOCAL_PKG']
     pkg = File.basename(ENV['LOCAL_PKG'])
@@ -18,22 +15,6 @@ test_name 'Install Bolt package' do
   else
     dev_builds_url = ENV['DEV_BUILDS_URL'] || 'http://builds.delivery.puppetlabs.net'
     sha_yaml_url = "#{dev_builds_url}/bolt/#{ENV['SHA']}/artifacts/#{ENV['SHA']}.yaml"
-    unless link_exists?(sha_yaml_url)
-      fail_test("<SHA>.yaml URL '#{sha_yaml_url}' does not exist.")
-    end
-
-    base_url, build_details = fetch_build_details(sha_yaml_url)
-    artifact_url, _ = host_urls(bolt, build_details, base_url)
-
-    case bolt['platform']
-      # TODO: BKR-1109 requests a supported way to install packages on Windows and OSX
-    when %r{windows}
-      generic_install_msi_on(bolt, artifact_url)
-    when %r{osx}
-      on bolt, "curl -O #{artifact_url}"
-      bolt.install_package('bolt*')
-    else
-      bolt.install_package(artifact_url)
-    end
+    install_from_build_data_url('bolt', sha_yaml_url, bolt)
   end
 end
